69af8541a76e77c31c79f9d3a76af3ebabac417f
thesunRider <suryasaradhi3@gmail.com>
2024-06-28 08:19:21 -0400

    Add test example and datasheet of transmitter

---
+++ transmitter/test.cpp
@@ -0,0 +1,22 @@
+//
+// LED blink example for PIC18F4520
+// Written by Ted Burke
+// Last updated 5-3-2013
+//
+ 
+#include <xc.h>
+ 
+#pragma config OSC=INTIO67,MCLRE=OFF,WDT=ON,LVP=OFF,BOREN=OFF
+ 
+int main(void)
+{
+    // Make RD0 a digital output
+    TRISD = 0b11111110;
+     
+    // Blink LED on RD0 (pin 19)
+    while(1)
+    {
+        LATDbits.LATD0 = 1 - LATDbits.LATD0;
+        _delay(100000);
+    }
+}
